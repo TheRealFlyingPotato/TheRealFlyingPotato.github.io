8aeeb411d57c9f3d617c94e26e3bb850bbb5b50a
LizytheRocker <stinkybuggus@gmail.com>
2019-09-07 09:21:09 -0500

    random-commander images

--- random_commander/script.js
+++ random_commander/script.js
@@ -1,13 +1,19 @@
 function gameOver()
 {
-  u = "https://api.scryfall.com/cards/search?q=t%3Alegend+t%3Acreature+f%3Ac+usd%3C10";
-  u = "https://api.scryfall.com/cards/search?q=t%3Alegend+t%3Acreature+f%3Ac+usd<10+id%3D5+r%3Ar";
+  u = "https://api.scryfall.com/cards/search?q=t%3Alegend+t%3Acreature+f%3Ac+usd%3C5";
+  // u = "https://api.scryfall.com/cards/search?q=t%3Alegend+t%3Acreature+f%3Ac+usd<10+id%3D5+r%3Ar";
   $.getJSON(u, function(result) {
     cList = result.data;
     console.log(cList);
-    for(i = 0; i < cList.length; i++) {
-      console.log(cList[i].name)
-      console.log(cList[i].image_uris.normal)
+    $("#imgs").empty();
+    for(i = 0; i < 2; i++) {
+      q = Math.floor(Math.random() * cList.length);
+      card = cList[q];
+      console.log(card)
+      cList.splice(q,1);
+      $("#imgs").append("<a href=\"" + card.scryfall_uri + "\"><img src= \"" + card.image_uris.normal + "\"/></a>");
+      // $("#imgs").append(card.image_uris.normal);
+      $("#imgs").append("<br>");
     }
   });
 }
