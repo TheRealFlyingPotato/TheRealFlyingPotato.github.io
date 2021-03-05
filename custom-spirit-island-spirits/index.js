c1468bed479966694006565d86626bcb9d312d58
TheRealFlyingPotato <the.real.mward@gmail.com>
2021-03-05 16:36:06 -0600

    looking better

--- custom-spirit-island-spirits/index.js
+++ custom-spirit-island-spirits/index.js
@@ -7,10 +7,12 @@ window.onload = function() {
   spirit_names.forEach((n) => {
     name_uri = n.replaceAll(' ','-').replaceAll("'","").toLowerCase();
     var path = "custom-spirits/" + name_uri + "/";
-    var spirit = $("<div class='spirit'></div>");
-    spirit.append("<h3>"+n+"</h3>");
-    spirit.append("<a href='"+path+"board.html'>Board</a><br/>");
-    spirit.append("<a href='"+path+"cards.html'>Cards</a>");
+    var spirit = $("<a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\" style=\"background-image:url("+path+"bg.png);background-position:center\">");
+    spirit.append("<h4>"+n+"</h4>");
+    // spirit.append("<ul>")
+    btn_class = 'btn btn-light btn-small'
+    spirit.append("<a class='"+btn_class+"' href='"+path+"board.html'>Board</a>");
+    spirit.append("<a class='"+btn_class+"' href='"+path+"cards.html'>Cards</a>");
     container.append(spirit);
   })
 };
