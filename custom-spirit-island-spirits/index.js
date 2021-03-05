75091b771a09382692b5f540bb618266c41ab8a9
TheRealFlyingPotato <the.real.mward@gmail.com>
2021-03-05 17:22:23 -0600

    spirits better centering

--- custom-spirit-island-spirits/index.js
+++ custom-spirit-island-spirits/index.js
@@ -7,10 +7,11 @@ window.onload = function() {
   spirit_names.forEach((n) => {
     name_uri = n.replaceAll(' ','-').replaceAll("'","").toLowerCase();
     var path = "custom-spirits/" + name_uri + "/";
-    var spirit = $("<a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\" style=\"background-image:url("+path+"bg.png);background-position:center\">");
+    var spirit = $("<a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\" style=\"background-image:linear-gradient(rgba(0, 0, 0, 0.25),rgba(0, 0, 0, 0.25)),url("+path+"bg.png);background-position:center\">");
     spirit.append("<h4>"+n+"</h4>");
     // spirit.append("<ul>")
-    btn_class = 'btn btn-light btn-small'
+    // btn_class = 'btn btn-danger btn-sm'
+    btn_class="btn"
     spirit.append("<a class='"+btn_class+"' href='"+path+"board.html'>Board</a>");
     spirit.append("<a class='"+btn_class+"' href='"+path+"cards.html'>Cards</a>");
     container.append(spirit);
