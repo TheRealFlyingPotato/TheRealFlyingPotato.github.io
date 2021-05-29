5609770654af04b218d476a792628fb0b1f0a6e4
TheRealFlyingPotato <the.real.mward@gmail.com>
2021-05-29 18:28:05 -0500

    board-games : heroku

--- board-games/index.js
+++ board-games/index.js
@@ -1,10 +1,15 @@
-// jQuery.ajaxPrefilter(function(options) {
-//   if (options.crossDomain && jQuery.support.cors) {
-//       options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-//   }
-// });
+jQuery.ajaxPrefilter(function(options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    options.url = 'https://pacific-reaches-88708.herokuapp.com/' + options.url;
+  }
+});
 
 $(document).ready( () => {
+  var logs = ['0.1: Added heroku for cors-anywhere'];
+  l = '';
+  for (const x of logs) { l = l + '\n' + x};
+  console.log(l);
   var url = 'https://boardgamegeek.com/collection/user/TheRealFlyingPotato?objecttype=thing&ff=1&subtype=boardgame'
   $.ajax({ url: url, success: function(data) { console.log(data); } });
 });
